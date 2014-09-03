884e1c2e05614065007147cc7984e371d65f1ca5
Riley Pinkerton <rpinkert@caltech.edu>
2014-09-03 00:00:20 -0700

    Added a function to the email script to allow @ruddock email aliases

--- update_mail.py
+++ update_mail.py
@@ -37,26 +37,54 @@ def getAdditionalEmails(lst):
   emails = connection.execute(query, lst=lst).fetchall()
   return emails
 
-# Connect to the mySQL database.
-engine = create_engine(config.DB_URI, convert_unicode=True)
-connection = engine.connect()
+if __name__ == "__main__":
 
-### First update email lists from table 'updating_email_lists' ###
-lists_query = text("SELECT listname, query FROM updating_email_lists")
-lists = connection.execute(lists_query).fetchall()
-# for each list, update!
-for (lst, query) in lists:
-  # perform query to get emails
-  results = connection.execute(text(query)).fetchall()
-  updateFromList(results, lst)
+  # Connect to the mySQL database.
+  engine = create_engine(config.DB_URI, convert_unicode=True)
+  connection = engine.connect()
 
-### Now, update email lists which correspond to an office ###
-lists_query = text("SELECT office_id, office_email FROM offices WHERE office_email IS NOT NULL")
-lists = connection.execute(lists_query).fetchall()
-# for each list, update!
-for (office_id, lst) in lists:
-  query = text("SELECT email \
-                FROM office_members_current NATURAL JOIN offices NATURAL JOIN members_current \
-                WHERE office_id = :oid")
-  results = connection.execute(query, oid=office_id).fetchall()
-  updateFromList(results, lst)
+  ### First update email lists from table 'updating_email_lists' ###
+  lists_query = text("SELECT listname, query FROM updating_email_lists")
+  lists = connection.execute(lists_query).fetchall()
+  # for each list, update!
+  for (lst, query) in lists:
+    # perform query to get emails
+    results = connection.execute(text(query)).fetchall()
+    updateFromList(results, lst)
+
+  ### Now, update email lists which correspond to an office ###
+  lists_query = text("SELECT office_id, office_email FROM offices WHERE office_email IS NOT NULL")
+  lists = connection.execute(lists_query).fetchall()
+  # for each list, update!
+  for (office_id, lst) in lists:
+    query = text("SELECT email \
+                  FROM office_members_current NATURAL JOIN offices NATURAL JOIN members_current \
+                  WHERE office_id = :oid")
+    results = connection.execute(query, oid=office_id).fetchall()
+    updateFromList(results, lst)
+
+def update_aliases():
+  """Updates postfix aliases for all users"""
+
+  # Connect to the mySQL database.
+  engine = create_engine(config.DB_URI, convert_unicode=True)
+  connection = engine.connect()
+
+  user_emails_query = text("select members.email, users.username from members join users on members.user_id=users.user_id")
+  user_emails = connection.execute(user_emails_query).fetchall()
+
+  # Get the current aliases in order to only add new users
+  current_alias_file = open("/etc/aliases_custom", "rb")
+  current_aliases = {}
+  for line in current_alias_file:
+    if ": " in line:
+      alias = tuple(line.strip("\n").split(": "))
+      current_aliases[alias[0]] = alias[1]
+  current_alias_file.close()
+
+  # Now write to the file
+  current_alias_file = open("/etc/aliases_custom", "ab")
+
+  for (email, user) in user_emails:
+    if user not in current_aliases:
+      current_alias_file.write("{0}: {1}\n".format(user, email))
