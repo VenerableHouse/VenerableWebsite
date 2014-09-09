1b2f8808caa8e4ab15876e81e6cccf7c7d004a81
Eric Pelz <eric+github@ericpelz.com>
2014-09-09 07:47:55 -0700

    Extend sendEmail helper to allow multiple to recipients

--- email_utils.py
+++ email_utils.py
@@ -5,6 +5,8 @@ from email.mime.text import MIMEText
 def sendEmail(to, msg, subject='A message from the Ruddock Website', \
     usePrefix=True):
   """ Sends an email to a user. """
+
+  to_list = to if isinstance(to, list) else [to]
   msg = MIMEText(msg)
 
   if usePrefix and '[RuddWeb]' not in subject:
@@ -12,8 +14,8 @@ def sendEmail(to, msg, subject='A message from the Ruddock Website', \
 
   msg['Subject'] = subject
   msg['From'] = 'auto@ruddock.caltech.edu'
-  msg['To'] = to
+  msg['To'] = ", ".join(to_list)
 
   s = smtplib.SMTP('localhost')
-  s.sendmail('auto@ruddock.caltech.edu', [to], msg.as_string())
+  s.sendmail('auto@ruddock.caltech.edu', to_list, msg.as_string())
   s.quit()
