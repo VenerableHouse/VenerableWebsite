a6c10a2aea6fbbe9a63158e7e73867faee15dc70
Daniel Kong <dkong1796@gmail.com>
2015-05-02 23:43:32 -0700

    Style changes to auth_utils.py
    - Fixing flask/sqlalchemy import and docstring conventions

--- run_server.py
+++ run_server.py
@@ -1,8 +1,12 @@
-# Note: this file is only used for testing locally. The production environment
-# uses wsgi to start up, and bypasses this file. So we are free to have debug
-# settings enabled.
-from RuddockWebsite import app, config
+"""
+Starts an instance of the test server for testing locally. The production
+enviornment uses the wsgi script to start up and bypasses this file, so we are
+free to have debug settings enabled.
+"""
+from RuddockWebsite import app
+from RuddockWebsite import config
 
-test_port = getattr(config, 'TEST_PORT', 5000)
-debug = getattr(config, 'DEBUG', True)
-app.run(debug=debug, port=test_port)
+if __name__ == "__main__":
+  test_port = getattr(config, 'TEST_PORT', 5000)
+  debug = getattr(config, 'DEBUG', True)
+  app.run(debug=debug, port=test_port)
