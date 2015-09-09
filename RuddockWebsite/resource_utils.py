23e32d8360044202abd48c652b8bdaa58cc3049e
Daniel Kong <dkong1796@gmail.com>
2015-09-09 00:34:03 -0700

    Created EnumValue class
    - To be used for enum values.
    - Similar to integers (can be compared directly to integers) but can
      contain additional metadata.

---
+++ RuddockWebsite/resource_utils.py
@@ -0,0 +1,28 @@
+class EnumValue():
+  """
+  EnumValue objects are meant to be used as elements of enums. They behave like
+  integers, but can contain additional data as well since they are extendable
+  objects. The equality and inequality operators are implemented, but other
+  comparison operators are not because it does not make sense to do less
+  than/greater than comparisons with enum values.
+  """
+  def __init__(self, value, **kwargs):
+    """
+    Sets value of the enum to the provided value. Can also set other attributes
+    using kwargs.
+    """
+    self._value = value
+    for key in kwargs:
+      setattr(self, key, kwargs[key])
+
+  def __eq__(self, other):
+    return self._value == other
+
+  def __ne__(self, other):
+    return not self.__eq__(other)
+
+  def __str__(self):
+    return str(self._value)
+
+  def __repr__(self):
+    return repr(self._value)
---
+++ RuddockWebsite/resources.py
@@ -0,0 +1,8 @@
+"""
+Use this module to contain static data. Static data is directly used by the
+codebase and should never be changed without a commit, as opposed to data
+stored in the database that can change at any moment and whose values are not
+necessarily constant.
+"""
+
+from RuddockWebsite.resource_utils import EnumValue
