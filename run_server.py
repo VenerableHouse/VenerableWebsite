62559e2707a49dbf43d6d83d4342786541f0e2b1
Daniel Kong <dkong1796@gmail.com>
2016-02-02 00:47:55 -0800

    Minor cleanup of run_server.py

--- run_server.py
+++ run_server.py
@@ -2,16 +2,16 @@
 
 import argparse
 import ruddock
-from ruddock import app
 
 parser = argparse.ArgumentParser(
     description="Run a local instance of the test server.")
 parser.add_argument("--env", default="dev",
-    help="Environment to run application in. Can be 'prod' or 'dev'.")
+    help="Environment to run application in. Can be 'prod' or 'dev'. "
+        + "Default is 'dev'.")
 parser.add_argument("--port", type=int, default=5000,
-    help="Port to attach application to.")
+    help="Port to attach application to. Default is 5000.")
 
 if __name__ == "__main__":
   args = parser.parse_args()
   ruddock.init(args.env)
-  app.run(port=args.port)
+  ruddock.app.run(port=args.port)
