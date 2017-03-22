f33260c50d60085ffe8348b657a9ee08822863ad
Henry Swanson <henry@silicon>
2017-03-22 15:17:58 -0700

    Added some documentation
    All routes and helper functions should now be documented.

--- ruddock/modules/rotation/routes.py
+++ ruddock/modules/rotation/routes.py
@@ -10,6 +10,7 @@ from ruddock.modules.rotation import blueprint, helpers
 @blueprint.route('/')
 @login_required(Permissions.ROTATION)
 def show_portal():
+  """The portal for all rotation pages."""
   dinner_list = helpers.DINNERS
   buckets = helpers.BUCKETS
   return flask.render_template('portal.html',
@@ -18,6 +19,7 @@ def show_portal():
 @blueprint.route('/directory')
 @login_required(Permissions.ROTATION)
 def show_prefrosh_list():
+  """Shows a list of prefrosh, possibly restricted to a particular dinner."""
   dinner_id = flask.request.args.get('dinner_id')
   if dinner_id is None or int(dinner_id) not in helpers.DINNERS:
     prefrosh_list = helpers.get_all_prefrosh()
@@ -30,13 +32,16 @@ def show_prefrosh_list():
 @blueprint.route('/images/<int:prefrosh_id>')
 @login_required(Permissions.ROTATION)
 def serve_image(prefrosh_id):
+  """Retrieves a prefrosh's picture."""
   img_contents = helpers.get_image_contents(prefrosh_id)
   return flask.Response(response=io.BytesIO(img_contents), status='200 OK',
             headers=None, mimetype='image/jpeg', content_type='image/jpeg')
 
+#TODO(henry) fail gracefully when prefrosh has no dinner
 @blueprint.route('/prefrosh/<int:prefrosh_id>')
 @login_required(Permissions.ROTATION)
 def show_prefrosh(prefrosh_id):
+  """Shows a particular prefrosh, their votes, and comments."""
   dinner_prefrosh = helpers.get_dinner_prefrosh_by_prefrosh_id(prefrosh_id)
   prefrosh, prev_id, next_id = helpers.get_prefrosh_and_adjacent(prefrosh_id, dinner_prefrosh)
   full_name = helpers.format_name(
@@ -48,6 +53,7 @@ def show_prefrosh(prefrosh_id):
 @blueprint.route('/update_info/<int:prefrosh_id>', methods=['POST'])
 @login_required(Permissions.ROTATION)
 def update_comment(prefrosh_id):
+  """Submission endpoint for updating feedback on a prefrosh."""
   helpers.update_comments(prefrosh_id, flask.request.form.get("comments", ""))
   helpers.update_votes(prefrosh_id, flask.request.form)
   return flask.redirect(flask.url_for("rotation.show_prefrosh", prefrosh_id=prefrosh_id))
@@ -55,6 +61,7 @@ def update_comment(prefrosh_id):
 @blueprint.route('/move')
 @login_required(Permissions.ROTATION)
 def move():
+  """The move up / move down interface."""
   old_bucket_name = flask.request.args.get("old_bucket_name")
   new_bucket_name = flask.request.args.get("new_bucket_name")
   if old_bucket_name not in helpers.BUCKETS or new_bucket_name not in helpers.BUCKETS:
@@ -73,6 +80,7 @@ def move():
 @blueprint.route('/move/change_bucket/<int:prefrosh_id>', methods=['POST'])
 @login_required(Permissions.ROTATION)
 def change_bucket(prefrosh_id):
+  """Submission endpoint for moving prefrosh between buckets."""
   new_bucket_name = flask.request.form.get("newBucket")
   old_bucket_name = flask.request.form.get("oldBucket")
   if old_bucket_name not in helpers.BUCKETS or new_bucket_name not in helpers.BUCKETS:
