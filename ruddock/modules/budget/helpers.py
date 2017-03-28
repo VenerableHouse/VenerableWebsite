772777517253df1a7de04731133e359c450a2d87
Henry Swanson <henry@silicon>
2017-03-28 14:27:26 -0700

    Mostly reorganization
    Changed postprocess to stringify, which takes specific column names.
    
    Better naming conventions.
    
    Added a page for displaying unpaid expenses. Will allow payment later.
    
    Started working on validation (only client-side for now, gotta figure
    out a clean way of doing server-side stuff).

--- ruddock/modules/budget/helpers.py
+++ ruddock/modules/budget/helpers.py
@@ -1,18 +1,36 @@
 import flask
 import sqlalchemy
+import enum
 
-def postprocess(db_rows):
-  """Turns every record in a row to a string."""
+# Enum for payment types
+class PaymentTypes(enum.Enum):
+  CASH = "Cash"
+  CHECK = "Check"
+  DEBIT = "Debit"
+  ONLINE = "Online"
+  TRANSFER = "Transfer"
+  OTHER = "Other"
 
-  # Turns the ResultProxy into a list of dictionaries
+def stringify(db_rows, cols):
+  """Changes the specified columns to strings.
+     This is especially useful when working with NUMERIC, because
+     JSON refuses to serialize Decimal objects.
+     This has the side effect of replacing the RowProxy objects with
+     dictionaries, but hopefully no method should notice... :/ """
+
+  # TODO drop cols argument and stringify everything?
+  # or maybe have it detect numeric columns?
+
   rows = [dict(r.items()) for r in db_rows]
 
-  # We have to convert NUMERIC to string, because it's not JSON serializable
-  for row in rows:
-    for k in row:
-      row[k] = str(row[k]) #TODO make it more precise in what it stringifies
+  for r in rows:
+    for k in cols:
+      r[k] = str(r[k])
 
   return rows
+
+def get_payment_types():
+  return [x.value for x in PaymentTypes]
 
 def get_fyears():
   """Gets list of all fiscal years."""
@@ -22,21 +40,29 @@ def get_fyears():
     ORDER BY fyear_id DESC
     """)
 
-  results = flask.g.db.execute(query).fetchall()
+  return flask.g.db.execute(query).fetchall()
 
-  return postprocess(results)
+def get_fyear_from_num(fyear_num):
+  """Looks up the record for the given year.
+     If fyear_num is None, then this returns the current year.
+     If no matching row is found, returns None."""
 
-def get_current_fyear():
-  """Returns the current fiscal year."""
-  query = sqlalchemy.text("""
-    SELECT fyear_id, fyear_num
-    FROM budget_fyears
-    WHERE CURDATE() BETWEEN start_date AND end_date
-  """)
+  if fyear_num is None:
+    query = sqlalchemy.text("""
+      SELECT fyear_id, fyear_num
+      FROM budget_fyears
+      WHERE CURDATE() BETWEEN start_date AND end_date
+    """)
+    rp = flask.g.db.execute(query)
+  else:
+    query = sqlalchemy.text("""
+      SELECT fyear_id, fyear_num
+      FROM budget_fyears
+      WHERE fyear_num = (:f)
+    """)
+    rp = flask.g.db.execute(query, f=fyear_num)
 
-  results = flask.g.db.execute(query).fetchone()
-
-  return postprocess([results])[0]
+  return rp.first()
 
 def get_budget_list(fyear_id):
   """Gets list of all budgets in the given year."""
@@ -48,9 +74,106 @@ def get_budget_list(fyear_id):
     ORDER BY budget_name
     """)
 
+  return flask.g.db.execute(query, f=fyear_id).fetchall()
+
+def get_expenses():
+  """Gets list of all expenses."""
+  query = sqlalchemy.text("""
+    SELECT budget_name, fyear_num, date_incurred, description, cost, payee_name
+    FROM budget_expenses
+      NATURAL JOIN budget_budgets
+      NATURAL JOIN budget_fyears
+      NATURAL LEFT JOIN budget_payees
+    ORDER BY expense_id DESC
+    """)
+
+  results = flask.g.db.execute(query).fetchall()
+
+  return stringify(results, ['cost'])
+
+def get_payments():
+  """Gets list of all payments."""
+  query = sqlalchemy.text("""
+    SELECT account_name, type, amount, date_written, date_posted, payee_name, check_no
+    FROM budget_payments
+      NATURAL JOIN budget_accounts
+      NATURAL LEFT JOIN budget_payees
+    ORDER BY payment_id DESC
+    """)
+
+  results = flask.g.db.execute(query).fetchall()
+
+  return stringify(results, ['amount'])
+
+def get_accounts():
+  """Gets list of all accounts."""
+  query = sqlalchemy.text("""
+    SELECT account_id, account_name
+    FROM budget_accounts
+    ORDER BY account_name
+    """)
+
+  return flask.g.db.execute(query).fetchall()
+
+def get_payees():
+  """Gets list of all payees."""
+  query = sqlalchemy.text("""
+    SELECT payee_id, payee_name
+    FROM budget_payees
+    ORDER BY payee_name
+    """)
+
+  return flask.g.db.execute(query).fetchall()
+
+def get_account_summary():
+  """Gets the status of all accounts."""
+  query = sqlalchemy.text("""
+    SELECT account_name,
+      initial_balance - IFNULL(SUM(IF(ISNULL(date_posted), 0, amount)), 0) AS bal,
+      initial_balance - IFNULL(SUM(amount), 0) AS post_bal
+    FROM budget_accounts
+      NATURAL LEFT JOIN budget_payments
+    GROUP BY account_id
+    ORDER BY account_name
+    """)
+
+  results = flask.g.db.execute(query).fetchall()
+
+  return stringify(results, ['bal', 'post_bal'])
+
+def get_budget_summary(fyear_id):
+  """Gets the status of all budgets for the given fiscal year."""
+  query = sqlalchemy.text("""
+    SELECT budget_name,
+      starting_amount,
+      IFNULL(SUM(cost), 0) AS spent
+    FROM budget_budgets
+      NATURAL JOIN budget_fyears
+      NATURAL LEFT JOIN budget_expenses
+    WHERE fyear_id = (:f)
+    GROUP BY budget_id
+    ORDER BY budget_name
+    """)
+
   results = flask.g.db.execute(query, f=fyear_id).fetchall()
 
-  return postprocess(results)
+  return stringify(results, ['starting_amount', 'spent'])
+
+def get_unpaid_expenses():
+  """Gets all expenses without a corresponding payment."""
+  query = sqlalchemy.text("""
+    SELECT payee_name, budget_name, fyear_num, date_incurred, description, cost
+    FROM budget_expenses
+      NATURAL JOIN budget_budgets
+      NATURAL JOIN budget_fyears
+      NATURAL JOIN budget_payees
+    WHERE ISNULL(payment_id)
+    ORDER BY payee_id
+  """)
+
+  results = flask.g.db.execute(query).fetchall()
+
+  return stringify(results, ['cost'])
 
 def record_expense(budget_id, date_incurred, description, amount, payment_id, payee_id):
   """Inserts a new expense into the database."""
@@ -95,66 +218,13 @@ def record_payment(account_id, type, amount, date_written, date_posted, payee_id
 
   return result.lastrowid
 
-def get_account_summary():
-  """Gets the status of all accounts."""
+def record_new_payee(payee_name):
+  """Inserts a new payee into the database, returning its ID."""
   query = sqlalchemy.text("""
-    SELECT account_name,
-      initial_balance - IFNULL(SUM(IF(ISNULL(date_posted), 0, amount)), 0) AS bal,
-      initial_balance - IFNULL(SUM(amount), 0) AS post_bal
-    FROM budget_accounts
-      NATURAL LEFT JOIN budget_payments
-    GROUP BY account_id
-    ORDER BY account_name
-    """)
+    INSERT INTO budget_payees (payee_name)
+    VALUES (:p)
+  """)
 
-  results = flask.g.db.execute(query).fetchall()
+  result = flask.g.db.execute(query, p=payee_name)
 
-  return postprocess(results)
-
-def get_budget_summary(fyear_id):
-  """Gets the status of all budgets for the given fiscal year."""
-  query = sqlalchemy.text("""
-    SELECT budget_name,
-      starting_amount,
-      IFNULL(SUM(cost), 0) AS spent
-    FROM budget_budgets
-      NATURAL JOIN budget_fyears
-      NATURAL LEFT JOIN budget_expenses
-    WHERE fyear_id = (:f)
-    GROUP BY budget_id
-    ORDER BY budget_name
-    """)
-
-  results = flask.g.db.execute(query, f=fyear_id).fetchall()
-
-  return postprocess(results)
-
-def get_expenses():
-  """Gets list of all expenses."""
-  query = sqlalchemy.text("""
-    SELECT budget_name, fyear_num, date_incurred, description, cost, payee_name
-    FROM budget_expenses
-      NATURAL JOIN budget_budgets
-      NATURAL JOIN budget_fyears
-      NATURAL LEFT JOIN budget_payees
-    ORDER BY expense_id DESC
-    """)
-
-  results = flask.g.db.execute(query).fetchall()
-
-  return postprocess(results)
-
-def get_payments():
-  """Gets list of all payments."""
-  query = sqlalchemy.text("""
-    SELECT account_name, type, amount, date_written, date_posted, payee_name, check_no
-    FROM budget_payments
-      NATURAL JOIN budget_accounts
-      NATURAL LEFT JOIN budget_payees
-    ORDER BY payment_id DESC
-    """)
-
-  results = flask.g.db.execute(query).fetchall()
-
-  return postprocess(results)
-  
+  return result.lastrowid
--- ruddock/modules/budget/routes.py
+++ ruddock/modules/budget/routes.py
@@ -1,108 +1,116 @@
 import flask
+import httplib
 import json
+import itertools
 
 from ruddock.resources import Permissions
 from ruddock.decorators import login_required
 from ruddock.modules.budget import blueprint, helpers
 
 @blueprint.route('/')
-def portal():
+def route_portal():
   """Displays the budget portal."""
   return flask.render_template('portal.html')
 
 @blueprint.route('/summary')
-def show_summary():
+def route_summary():
   """Displays account and budget summaries."""
-  current_fyear = helpers.get_current_fyear()
-
+  current_fyear = helpers.get_fyear_from_num(None)
   fyears = helpers.get_fyears()
   a_summary = helpers.get_account_summary()
 
   return flask.render_template('summary.html',
     a_summary=a_summary, fyears=fyears, current_fyear_id=current_fyear["fyear_id"])
 
-@blueprint.route('/add_expense')
-def add_expense():
-  """Provides an interface for submitting an expense."""
-  # Check what year was provided
-  fyear_num = flask.request.args.get('fyear')
-
-  # If no year was provided, get the current year
-  if fyear_num is None:
-    fyear = helpers.get_current_fyear()
-    fyear_id = fyear["fyear_id"]
-    fyear_num = fyear["fyear_num"]
-  else:
-    # Look up the id of that fiscal year
-    fyears = helpers.get_fyears()
-    pred = lambda r : r["fyear_num"] == fyear_num
-    fyear_id = next((r["fyear_id"] for r in fyears if pred(r)), None)
-  
-  # If there is no such year
-  if fyear_id is None:
-    #TODO do something more informative!
-    flask.abort(404)
-
-  # Get the list of budgets for that year
-  budgets_list = helpers.get_budget_list(fyear_id)
-
-  return flask.render_template('add_expense.html',
-    fyear=fyear_num,
-    budgets=budgets_list)
-
-@blueprint.route('/add_expense/submit', methods=['POST'])
-def submit_expense():
-  """Sends the expense to the database."""
-  form = flask.request.form
-
-  # Extract the expense 
-  budget_id = form["budget_id"]
-  date_incurred = form["date_incurred"]
-  amount = form["amount"]
-  description = form["description"]
-  # checkboxes are dumb
-  defer_payment = form.get("defer_payment") is not None
-
-  # Are we deferring payment?
-  if defer_payment:
-    # If so, then we leave the payment_id field blank, but save the payee for bookkeeping
-    payment_id = None
-    payee_id = 1
-  else:
-    # If not, we need to submit a payment, so we can use it as our payment_id
-    # We have no need for the payee_id field though as well
-    # TODO transaction stuff?
-    payment_type = form["payment_type"]
-    account_id = form["account_id"]
-    check_no = form["check_no"]
-    payment_id = helpers.record_payment(account_id, payment_type, amount, date_incurred, date_incurred, None, check_no)
-
-    payee_id = None
-
-  # Record the expense
-  helpers.record_expense(budget_id, date_incurred, description, amount, payment_id, payee_id)
-
-  # Return the user to the same page, same fiscal year
-  fyear = flask.request.args.get("fyear")
-  return flask.redirect(flask.url_for("budget.add_expense", fyear=fyear))
-
 @blueprint.route('/expenses')
-def show_expenses():
+def route_expenses():
   """Displays list of expenses."""
   expenses = helpers.get_expenses()
 
   return flask.render_template('expenses.html', expenses=expenses)
 
 @blueprint.route('/payments')
-def show_payments():
+def route_payments():
   """Displays list of payments."""
   payments = helpers.get_payments()
 
   return flask.render_template('payments.html', payments=payments)
 
+@blueprint.route('/add_expense')
+def route_add_expense():
+  """Provides an interface for submitting an expense."""
+
+  # TODO allow for multiple years
+
+  # Get the lists for the dropdown menus
+  budgets_list = helpers.get_budget_list(2) #TODO don't leave this
+  payment_types = helpers.get_payment_types()
+  accounts = helpers.get_accounts()
+  payees = helpers.get_payees()
+
+  return flask.render_template('add_expense.html',
+    budgets=budgets_list,
+    payment_types=payment_types,
+    accounts=accounts,
+    payees=payees)
+
+@blueprint.route('/add_expense/submit', methods=['POST'])
+def route_submit_expense():
+  """Sends the expense to the database."""
+  form = flask.request.form
+
+  # Extract the form data
+  budget_id = form["budget-id"]
+  date_incurred = form["date-incurred"]
+  amount = form["amount"]
+  description = form["description"]
+  defer_payment = form.get("defer-payment") is not None # checkboxes are dumb
+  payee_id = form["payee-id"]
+  new_payee = form["new-payee"]
+  payment_type = form["payment-type"]
+  account_id = form["account-id"]
+  check_no = form["check-no"]
+
+  # TODO do some server-side validation too!
+
+  # This next part depends on whether we are deferring payment or not.
+  # If so, then we leave the payment ID null, cause it the corresponding payment
+  # hasn't been made yet. However, we require the payee. If it's a new payee,
+  # we have to insert it into the database first.
+  # If not, then we need to make a new payment, and use its ID. Payee ID is not
+  # needed.
+  # TODO transaction stuff?
+  if defer_payment:
+    payment_id = None
+    if new_payee:
+      payee_id = helpers.record_new_payee(new_payee)
+  else:
+    payee_id = None
+    payment_id = helpers.record_payment(account_id, payment_type, amount, date_incurred, date_incurred, None, check_no)
+
+  # Either way, record the expense and refresh the page
+  helpers.record_expense(budget_id, date_incurred, description, amount, payment_id, payee_id)
+  return flask.redirect(flask.url_for("budget.route_add_expense"))
+
+@blueprint.route('/unpaid')
+def route_unpaid():
+  """Displays unpaid expenses, and allows the user to create payments for them."""
+
+  # Wow, I love itertools
+  unpaid = helpers.get_unpaid_expenses()
+  unpaid_groups = itertools.groupby(unpaid, lambda x : x["payee_name"])
+
+  payment_types = helpers.get_payment_types()
+  accounts = helpers.get_accounts()
+
+  return flask.render_template('unpaid.html',
+    payment_types=payment_types,
+    accounts=accounts,
+    unpaid_groups=unpaid_groups)
+
 
 @blueprint.route('/ajax/budget_summary')
-def ajax_get_budget_summary():
+def ajax_budget_summary():
   fyear_id = flask.request.args.get('fyear_id')
   if fyear_id is None:
     #TODO respond better
