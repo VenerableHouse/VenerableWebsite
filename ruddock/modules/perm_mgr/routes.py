186926478eecc6ab99d1261827779a5a48cdf7dd
Siddharth Kurella <siddharthkurella@gmail.com>
2017-09-20 14:39:30 -0700

    Add clickable links to permission manager view
    Clicking the name of the office or user will lead to a page to edit
    permissions. TODO: implement edit page.

--- ruddock/modules/perm_mgr/routes.py
+++ ruddock/modules/perm_mgr/routes.py
@@ -12,10 +12,23 @@ from ruddock.modules.perm_mgr import blueprint, helpers
 def show_permissions():
   """Displays a list of permissions for current users and offices."""
   office_perms = [{"name": x["office_name"], 
-                   "perms": helpers.decode_perm_string(x['permissions'])} 
+                   "perms": helpers.decode_perm_string(x['permissions']),
+                   "id": x["office_id"]} 
                   for x in helpers.fetch_office_permissions()]
   user_perms = [{"name": x["name"], 
-                 "perms": helpers.decode_perm_string(x['permissions'])} 
+                 "perms": helpers.decode_perm_string(x['permissions']),
+                 "id": x["user_id"]} 
                 for x in helpers.fetch_user_permissions()]
   return flask.render_template('perm_list.html', office_perms=office_perms,
       user_perms=user_perms)
+
+@blueprint.route('/edit_user/<int:user_id>')
+@login_required(Permissions.PERMISSION_MANAGER)
+def edit_user_permissions(user_id):
+    pass
+
+@blueprint.route('/edit_office/<int:office_id>')
+@login_required(Permissions.PERMISSION_MANAGER)
+def edit_office_permissions(office_id):
+    pass
+
