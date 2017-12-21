5736c9c0242e0c3a6069e631ef46ae08c8aeb2cd
Arnav Sharma <asharma2@caltech.edu>
2017-12-20 17:13:50 -0800

    Add ability to edit/remove members

--- ruddock/modules/users/helpers.py
+++ ruddock/modules/users/helpers.py
@@ -34,10 +34,47 @@ def get_memberlist(search_type):
   return flask.g.db.execute(query).fetchall()
 
 def get_user_info(username):
-  """Retrieves a user's info."""
+  """Retrieves a user's info from their username."""
+  user_id = get_user_id_from_username(username)
+  if user_id is not None:
+    return get_member_info(user_id)
+  else:
+    return None
+
+def get_office_info(username):
+  """Procedure to get a user's officer info."""
+  query = sqlalchemy.text("""
+    SELECT office_name, start_date, end_date
+    FROM office_assignments NATURAL JOIN users NATURAL JOIN offices
+    WHERE username = :u
+    ORDER BY start_date, end_date, office_name
+  """)
+  return flask.g.db.execute(query, u=username).fetchall()
+
+def get_user_id_from_username(username):
+  """
+  Given a user's username, returns their user_id. Returns None if no user
+  with this username exists.
+  """
+  query = sqlalchemy.text('SELECT user_id FROM users WHERE username=:u')
+  user_id = flask.g.db.execute(query, u=username).first()['user_id']
+  if user_id is not None:
+    return int(user_id)
+  else:
+    return None
+
+def get_member_info(user_id):
+  """
+  Retrieves a member's info.
+  Returns None if no member with this user_id exists.
+  """
   query = sqlalchemy.text("""
     SELECT
-      username,
+      user_id,
+      uid,
+      first_name,
+      last_name,
+      preferred_name,
       name,
       email,
       phone,
@@ -52,43 +89,38 @@ def get_user_info(username):
     FROM members
       NATURAL JOIN members_extra
       NATURAL JOIN membership_types
-      NATURAL JOIN users
-    WHERE username=:u
+    WHERE user_id=:u
     """)
-  return flask.g.db.execute(query, u=username).first()
+  return flask.g.db.execute(query, u=user_id).first()
 
-def get_office_info(username):
-  """Procedure to get a user's officer info."""
+def remove_member(user_id):
+  """
+  Permanently removes a member from the members database, removing any
+  record of them from the website. Returns True on success and False otherwise.
+  Be careful with this!
+  """
   query = sqlalchemy.text("""
-    SELECT office_name, start_date, end_date
-    FROM office_assignments NATURAL JOIN users NATURAL JOIN offices
-    WHERE username = :u
-    ORDER BY start_date, end_date, office_name
-  """)
-  return flask.g.db.execute(query, u=username).fetchall()
-
-def get_member_info(user_id):
-  """Retrieves a member's info. Intended to be used in the case of a member not
-  having an account on the website, which would make get_user_info
-  impossible."""
-  query = sqlalchemy.text("""
-    SELECT
-      user_id,
-      uid,
-      first_name,
-      last_name,
-      preferred_name,
-      email,
-      member_type,
-      birthday,
-      matriculation_year,
-      graduation_year,
-      msc,
-      phone,
-      building,
-      room_number,
-      major
+    DELETE
     FROM members
-    WHERE user_id=:user_id
+    WHERE user_id=:u
     """)
-  return flask.g.db.execute(query, user_id=user_id).first()
+  # Return whether the query was successful or not. Since we're deleting
+  # on a primary key, we know that we only have to check for row count
+  # 1 or 0.
+  return flask.g.db.execute(query, u=user_id).rowcount == 1
+
+def edit_member_column(user_id, column, new_value):
+  """Edits a member using the new value given."""
+  # Using a string format instead of the sqlalchemy.text formatting method
+  # for the column name, because otherwise it will always have single quotes
+  # around it, which SQL doesn't like. The only more consistent way I can
+  # think of to do this is to use SQLAlchemy's built-in query objects instead
+  # of writing all the queries manually, but that involves changing a lot of
+  # existing code.
+  query = sqlalchemy.text("""
+    UPDATE members
+    SET {}=:new_val
+    WHERE user_id=:u
+    """.format(column))
+  # Same pattern as the return for remove_member.
+  return flask.g.db.execute(query, u=user_id, new_val=new_value).rowcount == 1
--- ruddock/modules/users/routes.py
+++ ruddock/modules/users/routes.py
@@ -2,10 +2,12 @@ import time
 import httplib
 import flask
 import sqlalchemy
+import sys
 
 from ruddock import auth_utils
 from ruddock.decorators import login_required
 from ruddock.modules.users import blueprint, helpers
+from ruddock.resources import Permissions
 
 @blueprint.route('/list', defaults={'search_type': 'all'})
 @blueprint.route('/list/<search_type>')
@@ -38,7 +40,9 @@ def view_profile(username):
       return flask.render_template('view_user.html',
           info=user_info,
           offices=offices,
-          strftime=time.strftime)
+          strftime=time.strftime,
+          edit_permission=auth_utils.check_permission(Permissions.USERS),
+          user_id=helpers.get_user_id_from_username(username))
   flask.abort(httplib.NOT_FOUND)
 
 @blueprint.route('/view_member/<user_id>')
@@ -52,8 +56,54 @@ def view_member(user_id):
         'view_user.html',
         info=member_info,
         # No user account means we can't call helpers.get_office_info().
-        # All officers should definitely have an account anyways.
+        # Hopefully, all officers have an account, although I still think
+        # this is something worth changing.
         offices=[],
-        strftime=time.strftime)
+        strftime=time.strftime,
+        edit_permission=auth_utils.check_permission(Permissions.USERS),
+        user_id=user_id)
   else:
     flask.abort(httplib.NOT_FOUND)
+
+@blueprint.route('/manage_member/user_id=<user_id>', methods=['GET', 'POST'])
+@login_required(Permissions.USERS)
+def manage_member(user_id):
+  """Procedure to edit a member in the database if the user browsing the
+  website has the permissions necessary to edit users (USERS or ADMIN)."""
+  member_info = helpers.get_member_info(user_id)
+  if flask.request.method == 'GET':
+    if user_id is not None:
+      return flask.render_template(
+          'manage_member.html',
+          user_id=user_id,
+          info=member_info)
+  else: # POST request, so they're submitting a form to edit/remove a user.
+    if 'remove' in flask.request.form:
+      if helpers.remove_member(user_id):
+        flask.flash('Success!')
+      else:
+        flask.flash('An error occurred trying to perform your operation. '
+                    'Please contact an IMSS Rep!')
+    elif 'edit' in flask.request.form:
+      # Everything comes back from the web page as a unicode string, but
+      # when we input the database query, some of these need to be passed
+      # as numbers instead. Note that user_id is stored in the database as
+      # an int as well, but it's not part of the dictionary, so we just cast
+      # it manually when we call helpers.edit_member_column.
+      stored_as_int = ('member_type', 'msc', 'room_number')
+      success = True
+
+      # use dict.items() instead in Python 3
+      for key, value in flask.request.form.iteritems():
+        if value != '' and key != 'edit':
+          if key in stored_as_int:
+            value = int(value)
+          if not helpers.edit_member_column(int(user_id), key, value):
+            success = False
+      if success:
+        flask.flash('Success!')
+      else:
+        flask.flash('An error occurred trying to perform your operation. '
+                    'Please contact an IMSS Rep!')
+    return show_memberlist('all')
+  flask.abort(httplib.NOT_FOUND)
