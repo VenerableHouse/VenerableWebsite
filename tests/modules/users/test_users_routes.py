ea747b9979028bac7f2c95ae9980b32fa64599a5
Arnav Sharma <asharma2@caltech.edu>
2018-03-21 12:32:17 -0700

    Add tests for manage member page

--- tests/modules/users/test_users_routes.py
+++ tests/modules/users/test_users_routes.py
@@ -8,6 +8,7 @@ import httplib
 
 from ruddock.testing import utils
 from ruddock.testing.fixtures import client
+from ruddock.resources import Permissions
 
 def test_show_memberlist(client):
   """Tests /members route."""
@@ -17,10 +18,27 @@ def test_show_memberlist(client):
   assert response.status_code == httplib.OK
 
 def test_view_member(client):
-  """Tests that viewing a user works."""
+  """Tests that viewing a member works."""
   user_id = 1
   with client.session_transaction() as session:
     utils.login(session)
   response = client.get(
       flask.url_for('users.view_member', user_id=user_id))
   assert response.status_code == httplib.OK
+
+def test_manage_member(client):
+  """Tests that a user with the USERS permission can view the page for
+     editing another member."""
+  user_id = 1
+  with client.session_transaction() as session:
+    utils.login(session)
+    utils.add_permission(session, Permissions.USERS)
+  response = client.get(flask.url_for('users.manage_member', user_id=user_id))
+  assert response.status_code == httplib.OK
+
+def test_forbidden_manage_member(client):
+  user_id = 1
+  with client.session_transaction() as session:
+    utils.login(session)
+  response = client.get(flask.url_for('users.manage_member', user_id=user_id))
+  assert response.status_code == httplib.FORBIDDEN
