6c4dc54d71e05177c5ebd9bb35fa0951c4ec9706
Hannah Fisher <hannahmaryfisher@gmail.com>
2021-08-26 22:01:15 -0700

    Add cart code to info.html, update default_secrets.

--- ruddock/default_secrets.py
+++ ruddock/default_secrets.py
@@ -3,6 +3,7 @@ Copy this file to secrets.py and set the actual values
 """
 
 DOOR_CODES = {'Room': '1-2-3'}
+CART_CODES = {'Cart': '1-2-3'}
 ACCOUNTS = {
     'Netflix': {
         'email': 'bfrink@ruddock.caltech.edu',
@@ -10,3 +11,8 @@ ACCOUNTS = {
     }
 }
 PHONES = {'1.5': '1234'}
+
+LINKS = {
+    'ExComm_Meeting_Minutes':'https://www.google.com',
+    'ExComm_Materials':'https://www.caltech.edu'
+}
