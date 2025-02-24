3e5db576919d13273e4bdc0eb9367bbadb333901
amritapasu <75818587+amritapasu@users.noreply.github.com>
2025-02-23 19:45:17 -0800

    budget viewing perms

--- ruddock/auth_utils.py
+++ ruddock/auth_utils.py
@@ -372,6 +372,9 @@ def generate_admin_links():
   if check_permission(Permissions.BUDGET):
     links.append(AdminLink('Budget',
       flask.url_for('budget.route_portal', _external=True)))
+  if check_permission(Permissions.BUDGET_VIEW):
+    links.append(AdminLink('Budget',
+      flask.url_for('budget.route_portal', _external=True)))
   return links
 
 def is_full_member(username):
